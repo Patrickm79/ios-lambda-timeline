e8bf40be037967151a32b2930bc01eca43755095
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-02 17:49:30 -0500

    Added AudioRecordingVC w/ functionality, + Constructed Storyboard
    Built out actions, outlets, and wired up to storyboard, some basic functionality for recording. 
    TODO: Set up delegate method to hand the chosen recording back to the post VC && add visualizer to the recording VC

---
+++ LambdaTimeline/View Controllers/AudioRecordingVC.swift
@@ -0,0 +1,251 @@
+//
+//  AudioRecordingVC.swift
+//  LambdaTimeline
+//
+//  Created by Patrick Millet on 6/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AudioRecordingVC: UIViewController {
+    
+    
+    private var timer: Timer?
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var timeElapsedLabel: UILabel!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet var timeSlider: UISlider!
+    
+    private lazy var timeIntervalFormatter: DateComponentsFormatter = {
+        
+        let formatting = DateComponentsFormatter()
+        formatting.unitsStyle = .positional
+        formatting.zeroFormattingBehavior = .pad
+        formatting.allowedUnits = [.minute, .second]
+        return formatting
+    }()
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timeElapsedLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeElapsedLabel.font.pointSize,
+                                                                 weight: .regular)
+        timeRemainingLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeRemainingLabel.font.pointSize,
+                                                                   weight: .regular)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        try? prepareAudioSession()
+        updateViews()
+    }
+    
+    deinit {
+        cancelTimer()
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.030, repeats: true) { [weak self] (_) in
+            guard let self = self else { return }
+            
+            self.updateViews()
+        }
+    }
+    
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    
+    private func updateViews() {
+        playButton.isSelected = isPlaying
+        recordButton.isSelected = isRecording
+        
+        let elapsedTime = audioPlayer?.currentTime ?? 0
+        let duration = audioPlayer?.duration ?? 0
+        let timeRemaining: TimeInterval = round(duration) - elapsedTime
+        
+        timeElapsedLabel.text = timeIntervalFormatter.string(from: elapsedTime)
+        timeRemainingLabel.text = timeIntervalFormatter.string(from: timeRemaining)
+        
+        timeSlider.minimumValue = 0
+        timeSlider.maximumValue = Float(duration)
+        timeSlider.value = Float(elapsedTime)
+    }
+    
+    
+    // MARK: - Playback
+    
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            audioPlayer?.delegate = self
+        }
+    }
+    
+    var isPlaying: Bool {
+        return audioPlayer?.isPlaying ?? false
+    }
+    
+func prepareAudioSession() throws {
+     let session = AVAudioSession.sharedInstance()
+     try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+     try session.setActive(true, options: [])
+    
+     }
+    
+    func togglePlayback() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func play() {
+        audioPlayer?.play() 
+        startTimer()
+        updateViews()
+    }
+    
+    func pause() {
+        audioPlayer?.pause()
+        cancelTimer()
+        updateViews()
+    }
+    
+    
+    // MARK: - Recording
+    
+    func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+        
+        print("recording URL: \(file)")
+        
+        return file
+    }
+    
+    
+    func requestPermissionOrStartRecording() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                guard granted == true else {
+                    print("We need microphone access")
+                    return
+                }
+                
+                print("Recording permission has been granted!")
+                // NOTE: Invite the user to tap record again, since we just interrupted them, and they may not have been ready to record
+            }
+        case .denied:
+            print("Microphone access has been blocked.")
+            
+            let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
+        }
+    }
+    
+    func toggleRecording() {
+        if isRecording {
+            stopRecording()
+        } else {
+            requestPermissionOrStartRecording()
+        }
+    }
+    
+    func startRecording() {
+        let recordingURL = createNewRecordingURL()
+        //44,100 hertz = 44.1kHZ = FM / CD quality audio
+        let audioFormat = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        audioRecorder = try? AVAudioRecorder(url: recordingURL, format: audioFormat)
+        audioRecorder?.delegate = self
+        audioRecorder?.record()
+        self.recordingURL = recordingURL
+        updateViews()
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        updateViews()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func togglePlayback(_ sender: Any) {
+        togglePlayback()
+    }
+    
+    @IBAction func updateCurrentTime(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func toggleRecording(_ sender: Any) {
+        toggleRecording()
+    }
+}
+
+
+extension AudioRecordingVC: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Error decoding audio: \(error)")
+        }
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
+}
+
+
+extension AudioRecordingVC: AVAudioRecorderDelegate {
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            print("Error encoding recording audio: \(error)")
+        }
+        self.updateViews()
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if let recordingURL = recordingURL {
+        print("Finished recording and saving: \(recordingURL.path)")
+            
+            audioPlayer = try? AVAudioPlayer(contentsOf: recordingURL) // TODO: Errors
+        }
+        self.updateViews()
+    }
+}
